50324d070d7085a630be59f842c271bcb1681f08
Shuhei Kagawa <shuhei.kagawa@gmail.com>
2014-03-01 13:03:10 +0900

    Stop messing up files without front matter.

--- gulpfile.js
+++ gulpfile.js
@@ -20,7 +20,7 @@ var blogConfig = {
 gulp.task('copy', function () {
   return gulp.src(['./source/**/*', '!./source/_*/**/*'])
     .pipe(plumber())
-    .pipe(frontMatter())
+    .pipe(condition(__dirname + '/source/**/*.{markdown,md,textile}', frontMatter()))
     .pipe(condition(__dirname + '/source/**/*.{markdown,md}', markdown()))
     .pipe(blog.layout(blogConfig))
     .pipe(gulp.dest('./public'));
