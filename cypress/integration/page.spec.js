ca712203b27cc2d4ba2cf5877e1eabbb7c0de128
Shuhei Kagawa <shuhei.kagawa@gmail.com>
2019-12-21 20:35:38 +0100

    Stub Google Analytics and add more test cases

--- cypress/integration/page.spec.js
+++ cypress/integration/page.spec.js
@@ -1,21 +1,109 @@
-/* global cy */
+/* global Cypress, cy */
+
+// Stub Google Analytics
+// https://github.com/cypress-io/cypress-example-recipes/tree/master/examples/stubbing-spying__google-analytics
+Cypress.on("window:before:load", win => {
+  Object.defineProperty(win, "ga", {
+    writable: false,
+    value: cy.stub().as("ga")
+  });
+});
+
+const host = "http://localhost:4000";
+
 describe("shuheikagawa.com", () => {
-  it("opens top page", () => {
-    cy.visit("http://localhost:4000/");
+  beforeEach(() => {
+    cy.visit(`${host}/`);
+  });
+
+  it("sends a page view event to Google Analtics", () => {
+    cy.get("@ga")
+      .should("be.calledWith", "create", "UA-309586-8", "shuheikagawa.com")
+      .and("be.calledWith", "send", "pageview");
+  });
+
+  it("navigates to older and newer posts", () => {
+    cy.get("h1").should("have.length", 4);
+
+    cy.contains("Older Posts").click();
+    cy.url().should("eq", `${host}/blog/pages/2/`);
+    cy.get("h1").should("have.length", 4);
+
+    cy.contains("Older Posts").click();
+    cy.url().should("eq", `${host}/blog/pages/3/`);
+    cy.get("h1").should("have.length", 4);
+
+    cy.contains("Newer Posts").click();
+    cy.contains("Newer Posts").click();
+    cy.url().should("eq", `${host}/`);
+    cy.get("h1").should("have.length", 4);
   });
 
   it("opens about page", () => {
     cy.contains("About").click();
-    cy.url().should("include", "/about/");
+
+    cy.url().should("eq", `${host}/about/`);
+
+    cy.contains("Accounts").should("be.visible");
+    cy.contains("GitHub").should(
+      "have.prop",
+      "href",
+      "https://github.com/shuhei"
+    );
+    cy.contains("Twitter").should(
+      "have.prop",
+      "href",
+      "https://twitter.com/shuheikagawa"
+    );
+
+    cy.contains("Talks").should("be.visible");
   });
 
   it("opens works page", () => {
     cy.contains("Works").click();
-    cy.url().should("include", "/works/");
+
+    cy.url().should("eq", `${host}/works/`);
+
+    cy.contains("Code").should("be.visible");
+    cy.contains("GitHub").should(
+      "have.prop",
+      "href",
+      "https://github.com/shuhei"
+    );
+    cy.contains("npm").should(
+      "have.prop",
+      "href",
+      "https://www.npmjs.com/~shuhei"
+    );
+
+    cy.contains("Visible Things").should("be.visible");
+    cy.contains("Pixelm").should("have.prop", "href", `${host}/pixelm/`);
+    cy.contains("Compare").should(
+      "have.prop",
+      "href",
+      `https://github.com/shuhei/Compare`
+    );
   });
 
   it("opens archives page", () => {
     cy.contains("Archives").click();
-    cy.url().should("include", "/archives/");
+
+    cy.url().should("eq", `${host}/blog/archives/`);
+
+    cy.get(".post-list-item__title a")
+      .eq(0)
+      .then($postItem => {
+        const href = $postItem.prop("href");
+        const title = $postItem.text();
+
+        cy.get($postItem).click();
+
+        // Check the linked individual page.
+        cy.url().should("eq", href);
+        cy.get("h1")
+          .eq(1)
+          .invoke("text")
+          .should("contain", title);
+      });
   });
 });
