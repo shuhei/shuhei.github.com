50e0738bf6d9f91196017584d95970e0b2a548c3
Shuhei Kagawa <shuhei.kagawa@gmail.com>
2021-01-31 16:19:04 +0100

    Copy static files

--- .eleventy.js
+++ .eleventy.js
@@ -5,12 +5,21 @@ module.exports = config => {
   config.addCollection("posts", api => {
     return api.getFilteredByGlob("source/_posts/*.md").reverse();
   });
+
   config.addFilter("formatDate", formatDate);
   config.addFilter("insertWbr", insertWbr);
   config.addFilter("join", (items, separator) => items.join(separator));
+
+  // Paththrough copy files paths are relative to the project root.
+  config.addPassthroughCopy("source/images");
+  config.addPassthroughCopy("source/ng2src");
+  config.addPassthroughCopy("source/talks");
+  config.addPassthroughCopy("source/works");
+  config.addPassthroughCopy("source/.nojekyll");
+  config.addPassthroughCopy("source/CNAME");
+  config.addPassthroughCopy("source/favicon.ico");
+
   return {
-    // templateFormats: ["md", "njk"],
-    // htmlTemplateEngine: "njk",
     dir: {
       input: "./source",
       output: "./_site"
