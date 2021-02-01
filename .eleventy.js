324fccdd77b1c9e7ab682ed7fe451db4dce627e1
Shuhei Kagawa <shuhei.kagawa@gmail.com>
2021-02-01 22:55:15 +0100

    Minify HTML

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,4 @@
+const { htmlmin } = require("./lib/transformers/htmlmin");
 const { formatDate } = require("./lib/utils/date");
 const { insertWbr } = require("./lib/utils/wbr");
 const { applyPostcss } = require("./lib/plugins/css");
@@ -20,6 +21,8 @@ module.exports = config => {
   config.addFilter("join", (items, separator) => items.join(separator));
   config.addFilter("blogPermalink", blogPermalink);
   config.addFilter("indexPermalink", indexPermalink);
+
+  config.addTransform("htmlmin", htmlmin);
 
   // Paththrough copy files paths are relative to the project root.
   config.addPassthroughCopy("source/images");
