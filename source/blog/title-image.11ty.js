22ee5538fad5f4d3698e44279798a3a296fd3f8b
Shuhei Kagawa <shuhei.kagawa@gmail.com>
2021-12-31 22:18:52 +0100

    Refactor title image optimization

--- source/blog/title-image.11ty.js
+++ source/blog/title-image.11ty.js
@@ -1,5 +1,3 @@
-const Image = require("@11ty/eleventy-img");
-
 class TitleImage {
   data() {
     return {
@@ -16,30 +14,6 @@ class TitleImage {
 
   async render(data) {
     const page = data.pagination.items[0];
-    const image = page.data.image;
-    // Optimizing explicitly specified OG image here.
-    // TODO: Do this at a proper place.
-    if (image.startsWith("/images/") && image.endsWith(".jpg")) {
-      try {
-        const stats = await Image("source" + image, {
-          formats: ["jpeg"],
-          widths: [800],
-          urlPath: "/cached/",
-          outputDir: "./public/cached/"
-        });
-        if (stats.jpeg[0]) {
-          // TODO: Don't mutate...
-          page.data.image = stats.jpeg[0].url;
-          return Buffer.from([]);
-        }
-      } catch (err) {
-        console.error(
-          "[title-image] Failed to optimize title image",
-          image,
-          err
-        );
-      }
-    }
     return this.titleImage(page.data.title, data.site.twitter.subtitle);
   }
 }
